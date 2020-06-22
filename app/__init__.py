631c2cf332a6abdac1841826d2f5d53e16ebe504
Khoo De Hui <dehui@Khoos-MacBook-Pro.local>
2020-06-22 17:19:46 +0800

    Create login and registration form and functionality

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'd1d80829e8f23d626b011c280ddd2165'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'warning'
+
+from app import routes
---
+++ app/routes.py
@@ -0,0 +1,54 @@
+from flask import render_template, flash, url_for, redirect, request
+from flask_login import login_user, login_required, current_user, logout_user
+from app import app, db, bcrypt
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
+
+
+@app.route('/')
+@app.route('/start')
+def start():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    return render_template('start.html', title="Get Started")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            flash('Logged in successfully.', 'success')
+            next_page = request.args.get('next')
+            return redirect(next_page or url_for('home'))
+    return render_template('login.html', title='Log In', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Account created. You are now able to login.', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Sign Up', form=form)
+
+
+@app.route('/home')
+@login_required
+def home():
+    return render_template('home.html')
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash('Logged out successfully.', 'success')
+    return redirect(url_for('start'))
